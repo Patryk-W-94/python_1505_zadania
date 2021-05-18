a4d861d533524d6a520ffbae92dd80c9e4cbaefd
Patryk_Wiechec <patryk270@gmail.com>
2021-05-18 14:51:21 +0200

    zadanie1_2

---
+++ Zadanie1_2.py
@@ -0,0 +1,15 @@
+dni_tygodnia = ["poniedzialek", "wtorek", "sroda", "czwartek", "piatek", "sobota", "niedziela"]
+oddanie = input("W jaki dzien tygodnia oddales buty?: ")
+czas_naprawy = int(input("Ile dni potrwa naprawa?"))
+if oddanie in dni_tygodnia:
+    dzien_oddania = int(dni_tygodnia.index(oddanie))
+    if (dzien_oddania + czas_naprawy) < 7:
+        zwrot = dni_tygodnia[int(dzien_oddania + czas_naprawy)]
+        print(f"Dzien zwrotu butow wypada w {zwrot}")
+    elif dzien_oddania + czas_naprawy >= 7:
+        zwrot = dni_tygodnia[(int(dzien_oddania+czas_naprawy)-7)]
+        print(f"Dzien oddania butow wypada w {zwrot}")
+    else:
+        print("Wpisales zly dzien tygodnia!")
+
+
